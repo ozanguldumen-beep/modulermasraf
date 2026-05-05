4112d4a1daa09638d7715395c9c0d44eb32da2a8
ozanguldumen-beep <ozanguldumen@gmail.com>
2026-05-05 05:57:36 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Çalışan Bitrix24 webhook adresini buraya yapıştır.
+// Örnek:
+// const WEBHOOK = "https://modulerotomasyon.bitrix24.com.tr/rest/12/xxxxxxxxxxxx/";
+
+const WEBHOOK = "https://modulerotomasyon.bitrix24.com.tr/rest/12/0qho3np6gmo14p35/";
