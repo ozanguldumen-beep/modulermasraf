4989ab02291dd12b006f6504280726a39c6edb27
ozanguldumen-beep <ozanguldumen@gmail.com>
2026-05-05 06:02:52 +0300

    Delete masraf-app-ocr-resimli-son-hal directory

--- masraf-app-ocr-resimli-son-hal/config.js
+++
@@ -1,5 +0,0 @@
-// Çalışan Bitrix24 webhook adresini buraya yapıştır.
-// Örnek:
-// const WEBHOOK = "https://modulerotomasyon.bitrix24.com.tr/rest/12/xxxxxxxxxxxx/";
-
-const WEBHOOK = "https://modulerotomasyon.bitrix24.com.tr/rest/12/0qho3np6gmo14p35/";
